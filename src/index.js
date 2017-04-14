9a40b6b0bf63bbfdc2266df64a85a4cc1482b60f
Steve Gaeke <sgaeke@finchcomputing.com>
2017-04-14 17:04:44 -0400

    Added baseline files to begin experimenting.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+import {getUsers} from './api/userApi';
+
+getUsers().then(result => {
+
+  result.forEach(user => {
+    console.log(`${user.id} ${user.name}`);
+  });
+});
+
---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should have h1 that says Users', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+
+    // asyn call started...going to need use 'done'
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Users");
+      done(); // Need to call done for async calls
+      window.close();
+    });
+  })
+})
